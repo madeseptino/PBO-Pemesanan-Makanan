96db966cf1032ad068f775a79d81f2493938079b
SeptiNo <129021852+madeseptino@users.noreply.github.com>
2024-05-01 13:44:56 +0800

    Penambahan Menu Admin

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        Login.login();
+
+    public static void main (string[] args) {
+        int x = 123;
+
+        System.out.println(x);
     }
+
 }
